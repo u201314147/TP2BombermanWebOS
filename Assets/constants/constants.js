ce110c00a06324f6335601059a0d7b2b2caff0c6
u201314147 <u201314147@upc.edu.pe>
2020-10-22 07:41:50 -0500

    recov files

---
+++ Assets/constants/constants.js
@@ -0,0 +1,26 @@
+var floors = [
+    {
+        name: 'middleFloor',
+        isHorizontal: true
+    },
+    {
+        name: 'leftCornerDown',
+        isHorizontal: true
+    },
+    {
+        name: 'rightCornerDown',
+        isHorizontal: true
+    },
+    {
+        name: 'leftCornerUp',
+        isHorizontal: true
+    },
+    {
+        name: 'middleLeftWall',
+        isHorizontal: false
+    },
+    {
+        name: 'middleRightWall',
+        isHorizontal: false
+    }
+]
